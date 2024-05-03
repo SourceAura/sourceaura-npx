d9ab82ebfb05a5b8212935b822a2df282994d398
SourceAura <sourceaura@proton.me>
2024-05-02 21:00:27 -0500

    clearing and cleaning 🧹

--- callsign.js
+++ callsign.js
@@ -1,3 +1,7 @@
+// Import necessary dependencies
+import { default as clear } from 'clear';
+import { default as gradient } from 'gradient-string';
+
 // ASCII art text
 const tag = `
   ██████ ▒█████  █    ██ ██▀███  ▄████▄  ▄▄▄      █    ██ ██▀███  ▄▄▄      
@@ -12,21 +16,25 @@ const tag = `
                                 ░                                          
 `;
 
-
-
 // Gradient effect for text
 const gradientEffect = gradient([
-    { color: '#ff9966', pos: 0 },
-    { color: '#ff5e62', pos: 0.3 },
-    { color: '#ff66cc', pos: 0.6 },
-    { color: '#9966ff', pos: 1 },
-  ]);
-  
-  // Output the ASCII art text with gradient effect
-  // Function to display the ASCII art text with gradient effect
-  const callsign = () => {
-    // Clear the terminal screen
-    clear();
-    // Display the ASCII art text with gradient effect
-    console.log(gradientEffect(tag));
-  };
+  { color: '#0aa39f', pos: 0 }, 
+  { color: '#da6c80', pos: 0.6 },
+  { color: '#ff5e62', pos: 0.3 },
+  { color: '#ff9966', pos: 0.2 },
+  { color: '#9966ff', pos: 0.8 },
+  { color: '#9966ff', pos: 0.9 },
+  { color: '#542D6A', pos: 1 },
+]);
+
+// Function to display the ASCII art text with gradient effect
+const callsign = () => {
+  // Clear the terminal screen
+  clear();
+  // Display the ASCII art text with gradient effect
+  console.log(gradientEffect(tag));
+};
+
+// Export the callsign function
+export default callsign;
+
