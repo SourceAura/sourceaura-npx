5c65ec50942f1451885fca1e4f2d283d3178dddd
SourceAura <sourceaura.dev@gmail.com>
2024-07-13 21:31:58 -0500

    npx

--- callsign.js
+++ callsign.js
@@ -20,13 +20,13 @@ const tag = `
 
 // Gradient effect for text
 const gradientEffect = gradient([
-  { color: '#0aa39f', pos: 0 }, 
-  { color: '#da6c80', pos: 0.4 },
-  { color: '#ff5e62', pos: 0.4 },
-  { color: '#ff9966', pos: 0.7 },
-  { color: '#9966ff', pos: 0.8 },
-  { color: '#9966ff', pos: 0.9 },
-  { color: '#542D6A', pos: 1 },
+  { color: '#43AA88', pos: 0 }, 
+  { color: '#3CB0AF', pos: 0.3 },
+  { color: '#F6C309', pos: 0.4 },
+  { color: '#F19F20', pos: 0.7 },
+  { color: '#f85254', pos: 0.8 },
+  { color: '#900766', pos: 0.9 },
+  { color: '#50005B', pos: 1 },
 ]);
 
 // Function to display the ASCII art text with gradient effect
