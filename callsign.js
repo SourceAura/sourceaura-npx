004979394bb68b5396ce461dabb2f472079bbbbb
SourceAura <sourceaura.dev@gmail.com>
2024-07-30 21:30:12 -0500

    ...

--- callsign.js
+++ callsign.js
@@ -1,32 +1,31 @@
 // Import necessary dependencies
-import { default as clear } from 'clear';
-import { default as gradient } from 'gradient-string';
-
+import { default as clear } from "clear";
+import { default as gradient } from "gradient-string";
 
 // ASCII art text
 const tag = `
 
-██████    ▒█████    █   ██  ██▀███   ▄████▄  ▓█████ ▄▄▄       █    ██  ██▀███   ▄▄▄      
-▒██      ▒██▒  ██▒ ██  ▓██▒▓██   ██▒▒██▀ ▀█  ▓█   ▀▒████▄     ██  ▓██▒▓██   ██▒▒████▄    
-░  ██▄   ▒██░  ██▒▓██  ▒██░▓██  ▄█ ▒▒▓█    ▄ ▒███  ▒██  ▀█▄  ▓██  ▒██░▓██  ▄█ ▒▒██  ▀█▄  
-▒     ██▒▒██   ██░▓▓█  ░██░▒██▀▀█▄  ▒▓▓▄ ▄██▒▒▓█  ▄░██▄▄▄▄██ ▓▓█  ░██░▒██▀▀█▄  ░██▄▄▄▄██ 
+██████    ▒█████    █   ██  ██▀███   ▄████▄  ▓█████ ▄▄▄       █    ██  ██▀███   ▄▄▄
+▒██      ▒██▒  ██▒ ██  ▓██▒▓██   ██▒▒██▀ ▀█  ▓█   ▀▒████▄     ██  ▓██▒▓██   ██▒▒████▄
+░  ██▄   ▒██░  ██▒▓██  ▒██░▓██  ▄█ ▒▒▓█    ▄ ▒███  ▒██  ▀█▄  ▓██  ▒██░▓██  ▄█ ▒▒██  ▀█▄
+▒     ██▒▒██   ██░▓▓█  ░██░▒██▀▀█▄  ▒▓▓▄ ▄██▒▒▓█  ▄░██▄▄▄▄██ ▓▓█  ░██░▒██▀▀█▄  ░██▄▄▄▄██
 ▒██████▒▒░ ████▓▒░▒▒█████▓ ░██▓ ▒██▒▒ ▓███▀ ░░▒████▒▓█   ▓██▒▒▒█████▓ ░██▓ ▒██▒ ▓█   ▓██▒
 ▒ ▒▓▒ ▒ ░░ ▒░▒░▒░ ░▒▓▒ ▒ ▒ ░ ▒▓ ░▒▓░░ ░▒ ▒  ░░░ ▒░ ░▒▒   ▓▒█░░▒▓▒ ▒ ▒ ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░
 ░ ░▒  ░ ░  ░ ▒ ▒░ ░░▒░ ░ ░   ░▒ ░ ▒░  ░  ▒    ░ ░  ░ ▒   ▒▒ ░░░▒░ ░ ░   ░▒ ░ ▒░  ▒   ▒▒ ░
-░  ░  ░  ░ ░ ░ ▒   ░░░ ░ ░   ░░   ░ ░           ░    ░   ▒    ░░░ ░ ░   ░░   ░   ░   ▒   
+░  ░  ░  ░ ░ ░ ▒   ░░░ ░ ░   ░░   ░ ░           ░    ░   ▒    ░░░ ░ ░   ░░   ░   ░   ▒
       ░      ░ ░     ░        ░     ░ ░         ░  ░     ░  ░   ░        ░           ░  ░
-                                    ░                                                                                         
+                                    ░
 `;
 
-// Gradient effect for text
+// Gradient effect for text: Oasis 83''
 const gradientEffect = gradient([
-  { color: '#43AA88', pos: 0 }, 
-  { color: '#3CB0AF', pos: 0.3 },
-  { color: '#F6C309', pos: 0.4 },
-  { color: '#F19F20', pos: 0.7 },
-  { color: '#f85254', pos: 0.8 },
-  { color: '#900766', pos: 0.9 },
-  { color: '#50005B', pos: 1 },
+  { color: "#43AA88", pos: 0 },
+  { color: "#3CB0AF", pos: 0.3 },
+  { color: "#F6C309", pos: 0.4 },
+  { color: "#F19F20", pos: 0.7 },
+  { color: "#f85254", pos: 0.8 },
+  { color: "#900766", pos: 0.9 },
+  { color: "#50005B", pos: 1 },
 ]);
 
 // Function to display the ASCII art text with gradient effect
@@ -39,4 +38,3 @@ const callsign = () => {
 
 // Export the callsign function
 export default callsign;
-
